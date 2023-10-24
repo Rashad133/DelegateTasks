ca49a9ae56dd0129471e9471b511a322cfa08c16
Rashad133 <tu82907hw@code.edu.az>
2023-10-24 05:47:47 +0400

    my commit

---
+++ Delegate_Task2_24.10/Delegate_Task2_24.10/Program.cs
@@ -0,0 +1,23 @@
+﻿using Delegate_Task2_24._10.Models;
+
+namespace Delegate_Task2_24._10
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<Exam> exams = new List<Exam>
+            {
+                new Exam { Subject="Kimya",ExamDuration=2, StartDate = DateTime.Now },
+                new Exam { Subject="Riyaziyyat",ExamDuration=3, StartDate = DateTime.Now },
+                new Exam {Subject="Ingilis-dili",ExamDuration=1, StartDate = DateTime.Now },
+                new Exam {Subject="Fizika",ExamDuration=4,StartDate = DateTime.Now  },
+                new Exam {Subject="Az/dili",ExamDuration=2,StartDate = DateTime.Now  },
+            };
+
+
+        }
+    }
+}
---
+++ Delegates_Task1_24.10/Delegates_Task1_24.10/Program.cs
@@ -0,0 +1,49 @@
+﻿using Delegates_Task1_24._10.Models;
+using System;
+
+namespace Delegates_Task1_24._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person {Name = "Kamil", Surname="Sadigov", Age=23},
+                new Person {Name = "Rashad" , Surname="Rufullyev",Age=19 },
+                new Person {Name = "Alpay", Surname="Abdullayev",Age=20},
+                new Person {Name = "Ehmed",Surname="Osmanov",Age=24},
+                new Person {Name = "Murad" ,Surname="Qenberov",Age=22},
+            };
+
+            Console.WriteLine("Adi daxil edin");
+            string name = Console.ReadLine().ToLower();
+            List<Person> checkname = people.FindAll(x => x.Name.ToLower() == name);
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"{person.Name} {person.Surname}");
+            }
+            
+
+
+
+            List<Person> checksurname = people.FindAll(x => x.Surname.EndsWith("ov") || x.Surname.EndsWith("ova"));
+            Console.WriteLine("Soyadı 'ov' və 'ova' ile bitenler:");
+            foreach(Person person in checksurname)
+            {
+                Console.WriteLine($"{person.Name} {person.Surname }");
+            }
+            
+
+
+            List<Person> checkage = people.FindAll(x => x.Age > 20);
+            Console.WriteLine("Yashi 20-den boyuk olanlar:");
+            foreach (Person person in checkage)
+            {
+                Console.WriteLine($"{person.Name}{person.Surname} {person.Age}");
+            }
+           
+
+        }
+    }
+}
